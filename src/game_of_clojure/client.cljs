eaae8a7ce71b1c32c895966033cb076d44af5288
James Kent <jkent@chariotsolutions.com>
2020-04-21 22:30:56 -0400

    detects screen size to determine cell count

--- src/game_of_clojure/client.cljs
+++ src/game_of_clojure/client.cljs
@@ -3,30 +3,43 @@
             [reagent.dom :as rdom]
             [game-of-clojure.core :as core]))
 
+(defn calculate-count [dimension]
+  (-> dimension (/ 30) int dec))
+
+(defn discover-dimensions []
+  {:x-count (calculate-count (.-innerWidth js/window))
+   :y-count (calculate-count (.-innerHeight js/window))})
+
+(def dimensions (r/atom (discover-dimensions)))
+
+(defn on-resize [_]
+  (reset! dimensions (discover-dimensions)))
+
 (defonce living (r/atom core/r-pentomino))
 
 (defonce life (js/setInterval
                 #(swap! living core/tick) 500))
 
 (defn cell [{:keys [alive]}]
-  [:div {:style {:height          "30px"
-                 :width           "30px"
-                 :margin          "1px"
+  [:div {:style {:height          30
+                 :width           30
+                 :flex            1
+                 :margin          1
                  :backgroundColor (if alive "black" "wheat")}}])
 
 (defn row [{:keys [y living]}]
   [:div {:style {:display "flex"}}
    (map (fn [x] [cell {:key x :alive (contains? living [x y])}])
-        (range 40))])
+        (range (:x-count @dimensions)))])
 
-(defn home []
+(defn grid []
   [:div {:style {:display       "flex"
                  :flexDirection "column"}}
-   (let [living @living]
+   (let [living  @living]
      (map (fn [y] [row {:key y :y y :living living}])
-          (range 20)))
-   ])
+          (range (:y-count @dimensions))))])
 
 (defn ^:export run []
-  (rdom/render [home]
-               (js/document.getElementById "app")))
+  (rdom/render [grid]
+               (.getElementById js/document "app"))
+  (.addEventListener js/window "resize" on-resize))
